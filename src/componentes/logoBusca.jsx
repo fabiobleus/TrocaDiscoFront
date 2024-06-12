ab19a61946bf321eb0054e0aae89401bbc6a1738
Usuario <alanfaraj@outlook.com>
2024-06-12 14:13:03 -0300

    Fazendo o logotipo

---
+++ src/componentes/logoBusca.jsx
@@ -0,0 +1,14 @@
+const Logo = () => {
+
+    return (
+        <div>
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                   <img src="src\assets\logotipo.jpeg" width="80" height="80" alt="" />
+                </a>
+            </nav>
+        </div>
+    )
+}
+
+export default Logo;
