60879ebaa5ecc4ef4d577b4c4c7f709a4d6f61a6
Usuario <alanfaraj@outlook.com>
2024-06-12 16:30:28 -0300

    Fazendo logotipo e o menu

---
+++ src/componentes/menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = () => {
+
+    return (
+        <div>
+           <nav class="navbar navbar-expand-lg navbar-light bg-light"/>
+           <a class="navbar-brand" href="#">Home</a>
+        </div>
+    );
+};
+
+export default Menu;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Logo from './componentes/logoBusca.jsx'
+import Menu from './componentes/menu.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Logo/>
+    <Menu/>
   </React.StrictMode>,
 )
